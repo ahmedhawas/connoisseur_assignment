8137dbd57542a4ffa056767ca494bc0bf7a23f5b
ahmedhawas <ahmedhawas7@gmail.com>
2013-09-01 19:08:21 -0400

    commit before adding jon

--- router.rb
+++ router.rb
@@ -13,17 +13,18 @@ else
 	@categories =Array.new
 	@image_urls =Array.new
 	generator = HtmlGenerator.new(@product_names, @product_prices, @categories, @image_urls)
-	if action == "index"
+	if (action == "index")
 		@index_called = true # a variable used so that erb file knows what to print, the show values or the index values
+		@show_called=false 
 		generator.index
-	elsif action == "show"
+	elsif (action == "show")
 		@show_called=true 
+		@index_called = false
 		response = generator.show(ARGV[1].to_i)
-
 		@name = response["result"]["name"]
-		@price = parsed_response["result"]["regular_price_in_cents"]
-		@category = parsed_response["result"]["primary_category"]
-		@image_url = parsed_response["result"]["image_url"]
+		@price = response["result"]["regular_price_in_cents"]
+		@category = response["result"]["primary_category"]
+		@image_url = response["result"]["image_url"]
 	else
 		puts "Unknown action #{action}. Use index or show."
 	end
